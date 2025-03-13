272aa966a5e025660cfeb7abeb4688e6c2ce1c29
U-WALDRONSD\26phillips.korben <phillipsw1980@gmail.com>
2025-03-13 12:05:14 -0500

    Working on stuff

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,25 @@
-import { useState } from 'preact/hooks'
 import './app.css'
+
+const Heading = ({title, subtitle}) => {
+  return (
+    <>
+      <h3 class="text-white text-lg font-bold">{ title }</h3>
+      <h4 class="text-[#7aff76] text-md italic">{ subtitle }</h4>
+    </>
+  )
+}
 
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline"></h1>  
+    <>
+      <div class="bg-[#333] h-60 text-center flex flex-col items-center justify-center">
+        <h1 class="text-white text-5xl font-bold">SwiftSites Solutions</h1>
+        <h2 class="text-[#7aff76] text-xl italic mt-2">Empowering Your Online Presence</h2>
+      </div>
+      <div class="bg-[#444] p-8">
+        { Heading("Our Values", "And What We Stand For") }
+
+      </div>
+    </>
   )
 }
